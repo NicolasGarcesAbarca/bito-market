2988ef0925ce5e364f6d0a2ca1ba8497fd034bd0
Nicolas <nickogarces@gmail.com>
2023-08-06 21:00:27 -0400

    add audio to beat card

--- src/components/BeatCard.tsx
+++ src/components/BeatCard.tsx
@@ -1,6 +1,6 @@
 import { BeatProps } from "../services/beat"
 
-function BeatCard({ title, price, imageURL, author }: BeatProps) {
+function BeatCard({ title, price, imageURL, author, audioURL }: BeatProps) {
     return <div className="w-full flex flex-col shadow-lg">
         <div className="bg-gray-500 aspect-square">
             <img className="object-cover w-full aspect-square" src={imageURL} alt={title} />
@@ -10,6 +10,10 @@ function BeatCard({ title, price, imageURL, author }: BeatProps) {
             <p className="text-lg">{title}</p>
             <p className="text-sm text-gray-700">{author}</p>
         </div>
+        <audio controls>
+            <source src={audioURL} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+        </audio>
     </div>
 }
 
